bd6c8860fd649b1de291dc8a39d31f4fc1d4fd46
rei1091 <rei.bb.1091@gmail>
2023-08-30 14:06:00 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Footer } from '../components/Footer'
-import { Main } from '../components/Main'
-import { Header } from '../components/Header'
+import styles from 'src/styles/Home.module.css'
+import { Footer } from 'src/components/Footer'
+import { Main } from 'src/components/Main'
+import { Header } from 'src/components/Header'
 
 
 export default function Home() {
@@ -10,7 +10,6 @@ export default function Home() {
 		<div className={styles.container}>
 			<Head>
 				<title>About Page</title>
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
       <Header />
 			<Main page='about' />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Footer } from '../components/Footer'
-import { Main } from '../components/Main'
-import { Header } from '../components/Header'
+import styles from 'src/styles/Home.module.css'
+import { Main } from 'src/components/Main'
+import { Header } from 'src/components/Header'
+import { Footer } from 'src/components/Footer'
+
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index Paga</title>
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
       <Header />
 			<Main page='index' />
