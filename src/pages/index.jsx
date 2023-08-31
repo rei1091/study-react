507f412174fc446b2c22c105732c9f3280f7c361
rei1091 <rei.bb.1091@gmail>
2023-08-31 16:24:58 +0900

    ⚗️ 不要な記述だが、クリックイベントの確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,32 @@ import styles from 'src/styles/Home.module.css'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
 import { Footer } from 'src/components/Footer'
+import { useCallback } from 'react'
 
+//  const handleClick = (e, foo)=>{
+//  	console.log(e.target.href);
+// 	e.preventDefault();
+// 	alert(foo);
+//  };
 
 export default function Home() {
+	const foo = 1;
+
+	 const handleClick = useCallback((e)=>{
+	 	console.log(e.target.href);
+		e.preventDefault();
+		alert(foo);
+	 }, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index Paga</title>
 			</Head>
       <Header />
+			 <a href="/about" onClick={handleClick}
+			>
+			ボタン</a>
 			<Main page='index' />
 			<Footer />
 		</div>
