a2871daf34e3dba742d862dcce11314518abddee
rei1091 <rei.bb.1091@gmail>
2023-09-05 17:27:15 +0900

    ✨ useStateでm字列と真偽地について説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,8 @@ import {  useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
 	const [count, setCount] = useState(1);
-	// let foo = 1;
+	const [text, setText] = useState("");
+	const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
 		if(count <10){
@@ -17,6 +18,23 @@ export default function Home() {
 		//第二引数を指定し不要なレンダリングをさせない
 	}, [count]);
 
+	const handoleDisplay = useCallback(()=>{
+		setIsShow((isShow)=>
+			// if(isShow){
+			//   return false;
+			// }
+			 !isShow//isShow ? false: true;
+		);
+	}, []);
+
+	const handleChenge = useCallback((e)=>{
+		if(e.target.value.length > 5){
+			alert('5文字以内にしてください');
+			return;
+		}
+		setText(e.target.value.trim());
+	}, []);
+	
 	useEffect(()=> {
     document.body.style.backgroundColor ="lightblue";
 		return()=>{
@@ -31,10 +49,13 @@ export default function Home() {
 				<title>Index Paga</title>
 			</Head>
       <Header />
-			<h1>{count}</h1>
+			{isShow ? <h1>{count}</h1> : null}
 		  <button onClick={handleClick}
 			>
 			ボタン</button>
+			<button onClick={handoleDisplay}>
+			{isShow ? '非表示': '表示'}</button>
+			<input type='text' value={text} onChange={handleChenge}/>
 			<Main page='index' />
 			<Footer />
 		</div>
