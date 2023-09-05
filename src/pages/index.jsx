70d66578d9ca4c22e5c942f67823aa64784e1ab5
rei1091 <rei.bb.1091@gmail>
2023-09-05 16:41:34 +0900

    ⚡️ useCallnack,useEfectの第二引数に関して説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,30 +3,26 @@ import styles from 'src/styles/Home.module.css'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
 import { Footer } from 'src/components/Footer'
-import {  useEffect, useState } from 'react'
-
-//  const handleClick = (e, foo)=>{
-//  	console.log(e.target.href);
-// 	e.preventDefault();
-// 	alert(foo);
-//  };
+import {  useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
 	const [count, setCount] = useState(1);
 	// let foo = 1;
 
-  const handleClick = (e) => {
-		setCount((count)=>count+1);
-		setCount((count)=>count+1);
+  const handleClick = useCallback(() => {
+		if(count <10){
+			setCount((count)=>count+1);
+		}
 		// foo = foo+1;
-	};
+		//第二引数を指定し不要なレンダリングをさせない
+	}, [count]);
 
 	useEffect(()=> {
     document.body.style.backgroundColor ="lightblue";
 		return()=>{
 			document.body.style.backgroundColor ="";
 		}
-	},[])
+	},[]);
 
 
 	return (
