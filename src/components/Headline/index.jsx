c1ccb34e382486a4c640a02c6b02db2251e1a950
rei1091 <rei.bb.1091@gmail>
2023-09-06 18:07:14 +0900

    ✨ コンポーネント間のStateリフトアップ勉強

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -3,14 +3,17 @@ import classes from 'src/components/Headline/Headline.module.css';
 
 export function Headline(props) {
 
+	console.log(props.children)
 	return (
 		<div>
 			<h1 className={classes.title}>{props.page} page</h1>
 
-			<p className={classes.description}>
-				Get started by editing {props.children}
-			</p>
+			
+			<button onClick={props.handleReduce}>減らす</button>
 
+			<p className={classes.description}>
+				{props.children}
+			</p>
 		</div>
 	);
 }
